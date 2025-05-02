90306245789dd297115eb3415f32ad619ced6052
FlyingBaum <kristgsus@gmail.com>
2025-05-02 11:02:39 +0200

    refactor(eslint): adjust ignores and remove files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    ignores: ["p5/*", "**/Game_Uebung_11.js" ],
-  },
-  {
-    files: ['**/GameEngines Übung 11/**.js']
-  },
-  {
-    
-    languageOptions: { globals: globals.browser },
-},
-  pluginJs.configs.recommended,
+	{
+		ignores: ["p5/"],
+	},
+	{},
+	{
+		languageOptions: { globals: globals.browser },
+	},
+	pluginJs.configs.recommended,
+];
