8e37f911b6b772c55e9b81c10cc23f4ad271b0f4
Maximilian Ehlert <maximilian.ehlert@student.htw-berlin.de>
2025-05-10 21:31:51 +0200

    eslint plugin to make eslint recognise p5 methods

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import { p5globals, implicit } from "@chaitanyathakur/eslint-plugin-p5js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{
 		ignores: ["p5/"],  // Ignore files or directories you don't want to lint
+		
 	},
 	{
 		languageOptions: {
 			globals: {
 				...globals.browser,  // Include browser globals like `window`, `document`
+				...p5globals
 			},
 			parserOptions: {
 				ecmaVersion: 2020,     // Allow modern JS features like ES modules
 				sourceType: "module",  // Enable `import`/`export` syntax
 			},
 		},
+		rules: {
+			"no-unused-vars": ["warn", { varsIgnorePattern: implicit }],
+		  },
 	},
+	
 	pluginJs.configs.recommended,  // Use ESLint's recommended JS rules
 ];
