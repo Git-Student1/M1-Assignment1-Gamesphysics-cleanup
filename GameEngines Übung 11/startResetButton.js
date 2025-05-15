4a1faa17a0961ba27782560539dbd9f34d185d7c
FlyingBaum <kristgsus@gmail.com>
2025-05-15 12:45:47 +0200

    refactor: add constants to set buttonTextStart and buttonTextReset to

--- GameEngines Übung 11/startResetButton.js
+++ GameEngines Übung 11/startResetButton.js
@@ -1,49 +1,66 @@
-export default class StartResetButton{
-    buttonTextStart = "Start"
-    buttonTextReset = "Reset"
+const BUTTON_TEXT_START = "Start";
+const BUTTON_TEXT_RESET = "Reset";
 
-    constructor(){
-        this.buttonText = this.buttonTextStart;
-    }
-    inButton(mouseX, mouseY){
-        //console.log(""+mouseX+ " "+ mouseY)
-       let leftBorder = this.startbuttonCenterX -this.startbuttonWidth/2;
-       let rightBorder = this.startbuttonCenterX + this.startbuttonWidth/2;
-       let topBorder = this.startbuttonCenterY - this.startbuttonHeight/2;
-       let bottomBorder = this.startbuttonCenterY + this.startbuttonHeight/2;
+export default class StartResetButton {
+	constructor() {
+		this.buttonTextReset = BUTTON_TEXT_RESET;
+		this.buttonText = BUTTON_TEXT_START;
+	}
+	inButton(mouseX, mouseY) {
+		//console.log(""+mouseX+ " "+ mouseY)
+		let leftBorder = this.startbuttonCenterX - this.startbuttonWidth / 2;
+		let rightBorder = this.startbuttonCenterX + this.startbuttonWidth / 2;
+		let topBorder = this.startbuttonCenterY - this.startbuttonHeight / 2;
+		let bottomBorder = this.startbuttonCenterY + this.startbuttonHeight / 2;
 
-       
-       if(mouseX>leftBorder && mouseX<rightBorder){
-         if(mouseY>topBorder && mouseY<bottomBorder){
-           return true
-         }
-       }
-       return false
-      }
-    changeText(){
-        this.buttonText === this.buttonTextStart ? this.buttonText=this.buttonTextReset: this.buttonText= this.buttonTextStart;
-    }
-    draw(){
+		if (mouseX > leftBorder && mouseX < rightBorder) {
+			if (mouseY > topBorder && mouseY < bottomBorder) {
+				return true;
+			}
+		}
+		return false;
+	}
+	changeText() {
+		this.buttonText === this.buttonTextStart
+			? (this.buttonText = this.buttonTextReset)
+			: (this.buttonText = this.buttonTextStart);
+	}
+	draw() {
+		//button
+		rectMode(CENTER);
+		fill(200);
+		rect(
+			this.startbuttonCenterX,
+			this.startbuttonCenterY,
+			this.startbuttonWidth,
+			this.startbuttonHeight
+		);
 
-        //button
-        rectMode(CENTER)
-        fill(200)
-        rect(this.startbuttonCenterX,this.startbuttonCenterY, this.startbuttonWidth, this.startbuttonHeight)
-        
-        //button text
-        fill(0)
+		//button text
+		fill(0);
 
-        textSize(this.buttonTextSize)
-        //draw button texts
-        textAlign(CENTER, CENTER);
-        text(this.buttonText,this.startbuttonCenterX,this.startbuttonCenterY, this.startbuttonWidth, this.startbuttonHeight)
-    }
-    updateSize(startbuttonCenterX, startbuttonCenterY,startbuttonWidth,startbuttonHeight, buttonTextSize){
-        this.startbuttonCenterX = startbuttonCenterX;
-        this.startbuttonCenterY = startbuttonCenterY;
-        this.startbuttonWidth = startbuttonWidth;
-        this.startbuttonHeight = startbuttonHeight;
-        this.buttonTextSize = buttonTextSize;
-    }
-    
+		textSize(this.buttonTextSize);
+		//draw button texts
+		textAlign(CENTER, CENTER);
+		text(
+			this.buttonText,
+			this.startbuttonCenterX,
+			this.startbuttonCenterY,
+			this.startbuttonWidth,
+			this.startbuttonHeight
+		);
+	}
+	updateSize(
+		startbuttonCenterX,
+		startbuttonCenterY,
+		startbuttonWidth,
+		startbuttonHeight,
+		buttonTextSize
+	) {
+		this.startbuttonCenterX = startbuttonCenterX;
+		this.startbuttonCenterY = startbuttonCenterY;
+		this.startbuttonWidth = startbuttonWidth;
+		this.startbuttonHeight = startbuttonHeight;
+		this.buttonTextSize = buttonTextSize;
+	}
 }
