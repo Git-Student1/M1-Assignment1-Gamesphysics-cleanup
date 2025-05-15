ae882ba1f4b42dbf5744669708d99f050758e1d1
FlyingBaum <kristgsus@gmail.com>
2025-05-15 12:46:21 +0200

    refactor: assign state values in constructor

--- GameEngines Übung 11/states.js
+++ GameEngines Übung 11/states.js
@@ -1,40 +1,26 @@
-export default class State{
+export default class State {
+	constructor() {
+		this.ONSEESAW = "onSeesaw";
+		this.ONFLIGHT = "onFlight";
+		this.ONSLOPE = "onSlope";
 
-        
-        ONSEESAW = "onSeesaw"
-        ONFLIGHT = "onFlight"
-        ONSLOPE =  "onSlope"
+		this.state = this.ONSEESAW;
+	}
 
-        state = this.ONSEESAW
-    
-    constructor(){
-    }
+	changeStateTo(newState) {
+		if (!this._stateInStates(newState)) {
+			throw new Error("state is invalid: " + newState);
+		}
+		this.ballState = newState;
+	}
 
-    changeStateTo(newState){
-        if(!this.#stateInStates(newState)){
-            throw new Error("state is invalid: "+newState )
-        }
-        this.ballState = newState
-    }
-
-
-
-    /**
-     * returns whether the inserted parameter is one of the valid states
-     * @param {*} newState 
-     * @returns 
-     */
-    #stateInStates(newState){
-        if(Object.values(this.ballStates).includes(newState)) return true
-        return false
-
-    }
-
-    /**
-     * returns wether the stae can change from the current to the new one
-     * @param {*} newState 
-     */
-    #validStateChange(newState){
-        throw new Error("is still not implemented")
-    }
+	/**
+	 * returns whether the inserted parameter is one of the valid states
+	 * @param {*} newState
+	 * @returns
+	 */
+	_stateInStates(newState) {
+		if (Object.values(this.ballStates).includes(newState)) return true;
+		return false;
+	}
 }
